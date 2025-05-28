8037e596186050e2c28292fa11be7c6acf655b44
tdbester <142811772+tdbester@users.noreply.github.com>
2025-05-28 21:15:05 +0700

    config database

---
+++ src/main/java/Config/DBConnect.java
@@ -0,0 +1,18 @@
+package Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+
+
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=Talent_Center;encrypt=true;trustServerCertificate=true";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "12345678";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
+
