509c8e937124a008c326313d4399d746c79281f2
tdbester <142811772+tdbester@users.noreply.github.com>
2025-05-28 20:58:38 +0700

    init project

---
+++ src/main/java/Controller/BlogController.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class BlogController extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
